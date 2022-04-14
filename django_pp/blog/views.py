ffe8831fe2aadcadd9bbc43cb459f4fe9a2510fb
pmaevsk <maevskips@gmail.com>
2022-04-14 13:35:35 +0300

    6 stage user register added

--- django_pp/blog/views.py
+++ django_pp/blog/views.py
@@ -1,24 +1,10 @@
 from multiprocessing import context
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'PMaevski',
-        'title': 'Blog Post 1',
-        'content': 'First blog post ever',
-        'date_posted': 'April 10, 2022'
-    },
-    {
-        'author': 'TestUser',
-        'title': 'Blog Post 2',
-        'content': 'Some test text',
-        'date_posted': 'April 9, 2022'
-    },
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
